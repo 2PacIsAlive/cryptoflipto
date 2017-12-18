4ad481641b3ad3d9354b771fb2ea3b7660404ec8
Jared Jolton <jared.jolton@gmail.com>
2017-12-17 21:14:33 -0700

    update example

--- cryptoflipto/src/components/Editor.vue
+++ cryptoflipto/src/components/Editor.vue
@@ -83,41 +83,38 @@ export default {
   const btcQty = ltcQty * prices.ltcbtc.kraken
   const usdQty = btcQty * prices.btcusd.gdax 
   return {
-    opportunity: {
-      trades: [
-        {
-          pair: "ltcusd",
-          side: "buy",
-          exchange: "gdax",
-          cost: capital,
-          quantity: ltcQty,
-          price: prices.ltcusd.gdax
-        },{
-          pair: "ltcbtc",
-          side: "sell",
-          exchange: "kraken",
-          quantity: btcQty,
-          cost: ltcQty,
-          price: prices.ltcbtc.kraken
-        }, {
-          pair: "btcusd",
-          side: "sell",
-          exchange: "gdax",
-          quantity: usdQty,
-          cost: btcQty,
-          price: prices.btcusd.gdax
-        }
-      ],  
-      result: {
-        asset: "usd",
+    trades: [
+      {
+        pair: "ltcusd",
+        side: "buy",
+        exchange: "gdax",
+        cost: capital,
+        quantity: ltcQty,
+        price: prices.ltcusd.gdax
+      },{
+        pair: "ltcbtc",
+        side: "sell",
+        exchange: "kraken",
+        quantity: btcQty,
+        cost: ltcQty,
+        price: prices.ltcbtc.kraken
+      }, {
+        pair: "btcusd",
+        side: "sell",
+        exchange: "gdax",
         quantity: usdQty,
-        profit: usdQty - capital,
-        exchange: "gdax"
+        cost: btcQty,
+        price: prices.btcusd.gdax
       }
-    }
+    ],  
+    result: {
+      asset: "usd",
+      quantity: usdQty,
+      profit: usdQty - capital,
+      exchange: "gdax"
+    } 
   }
-}
-      `,
+}`,
       result: null,
       loadingResult: false,
       hasResult: false
